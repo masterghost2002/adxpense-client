dfef96717b267f36a98e572d7aa17ae3eb3c7d11
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2024-01-02 22:07:17 +0530

    added

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 const config = {
-    baseServerUrl:'https://server.masterghostcloud.cloud',
+    // baseServerUrl:'https://server.masterghostcloud.cloud',
+    baseServerUrl:'https://adarth-server.onrender.com'
     // baseServerUrl:'http://192.168.1.200:5000'
 }
 export default config;
